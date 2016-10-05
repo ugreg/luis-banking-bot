dff907fa8ff76a9871f728eb41e2eb9e981cc283
Greg Degruy <gregory.degruy@gmail.com>
2016-10-05 01:23:12 -0700

    Resolved System.Runtime.Serialization assembly

--- LUISBankingBot/Controllers/MessagesController.cs
+++ LUISBankingBot/Controllers/MessagesController.cs
@@ -7,12 +7,131 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 using Microsoft.Bot.Builder.Dialogs;
 using LUISBankingBot.Dialogs;
+using System.Threading;
+using System.Web;
+using System.Text;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.ComponentModel;
 
 namespace LUISBankingBot
 {
+    [DataContract]
+    public class AccessTokenInfo
+    {
+        [DataMember]
+        public string access_token { get; set; }
+        [DataMember]
+        public string token_type { get; set; }
+        [DataMember]
+        public string expires_in { get; set; }
+        [DataMember]
+        public string scope { get; set; }
+    }
+
+    public class Authentication
+    {
+        public static readonly string AccessUri = "https://oxford-speech.cloudapp.net/token/issueToken";
+        private string clientId;
+        private string clientSecret;
+        private string request;
+        private AccessTokenInfo token; // 774daf9ffd514e7dafeb592298812690
+        private Timer accessTokenRenewer;
+
+        //Access token expires every 10 minutes. Renew it every 9 minutes only.
+        private const int RefreshTokenDuration = 9;
+
+        public Authentication(string clientId, string clientSecret)
+        {
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+
+            //If clientid or client secret has special characters, encode before sending request
+            this.request = string.Format("grant_type=client_credentials&client_id={0}&client_secret={1}&scope={2}",
+                                              HttpUtility.UrlEncode(clientId),
+                                              HttpUtility.UrlEncode(clientSecret),
+                                              HttpUtility.UrlEncode("https://speech.platform.bing.com"));
+
+            this.token = HttpPost(AccessUri, this.request);
+
+            // renew the token every specfied minutes
+            accessTokenRenewer = new Timer(new TimerCallback(OnTokenExpiredCallback),
+                                           this,
+                                           TimeSpan.FromMinutes(RefreshTokenDuration),
+                                           TimeSpan.FromMilliseconds(-1));
+        }
+
+        //Return the access token
+        public AccessTokenInfo GetAccessToken()
+        {
+            return this.token;
+        }
+
+        //Renew the access token
+        private void RenewAccessToken()
+        {
+            AccessTokenInfo newAccessToken = HttpPost(AccessUri, this.request);
+            //swap the new token with old one
+            //Note: the swap is thread unsafe
+            this.token = newAccessToken;
+            Console.WriteLine(string.Format("Renewed token for user: {0} is: {1}",
+                              this.clientId,
+                              this.token.access_token));
+        }
+        //Call-back when we determine the access token has expired 
+        private void OnTokenExpiredCallback(object stateInfo)
+        {
+            try
+            {
+                RenewAccessToken();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("Failed renewing access token. Details: {0}", ex.Message));
+            }
+            finally
+            {
+                try
+                {
+                    accessTokenRenewer.Change(TimeSpan.FromMinutes(RefreshTokenDuration), TimeSpan.FromMilliseconds(-1));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Format("Failed to reschedule timer to renew access token. Details: {0}", ex.Message));
+                }
+            }
+        }
+
+        //Helper function to get new access token
+        private AccessTokenInfo HttpPost(string accessUri, string requestDetails)
+        {
+            //Prepare OAuth request 
+            WebRequest webRequest = WebRequest.Create(accessUri);
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.Method = "POST";
+            byte[] bytes = Encoding.ASCII.GetBytes(requestDetails);
+            webRequest.ContentLength = bytes.Length;
+            using (Stream outputStream = webRequest.GetRequestStream())
+            {
+                outputStream.Write(bytes, 0, bytes.Length);
+            }
+            using (WebResponse webResponse = webRequest.GetResponse())
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(AccessTokenInfo));
+                //Get deserialized object from JSON stream
+                AccessTokenInfo token = (AccessTokenInfo)serializer.ReadObject(webResponse.GetResponseStream());
+                return token;
+            }
+        }
+    }
+
     [BotAuthentication]
     public class MessagesController : ApiController
     {
