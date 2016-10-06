b9ec883b40f59b5b4c071a125d98b4342932e8c1
Greg Degruy <gregory.degruy@gmail.com>
2016-10-06 16:44:14 -0500

    Building out data structures for LuisModel
    Updated LUIS app export

--- LUISBankingBot/Dialogs/LuisDialog.cs
+++ LUISBankingBot/Dialogs/LuisDialog.cs
@@ -1,23 +1,42 @@
 ﻿namespace LUISBankingBot.Dialogs
 {
+    using LUISBankingBot.Models;
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Builder.Luis;
     using Microsoft.Bot.Builder.Luis.Models;
     using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     public class LuisBankingDialog
     {
-        // if want to test using http call
+        // http call
         // https://api.projectoxford.ai/luis/v1/application?id=6841d389-70d6-45ec-96a9-a2893d1c778e&subscription-key=5d7817feda724399aaf69441f3fb18eb&q={PUT_QUERY_TEXT_HERE}
         [LuisModel("6841d389-70d6-45ec-96a9-a2893d1c778e", "5d7817feda724399aaf69441f3fb18eb")]
         [Serializable]
         public class BankingDialog : LuisDialog<object>
         {
+            private LUISBankModel luisBankModel;
+
             [LuisIntent("Deposit")]
             public async Task DepositHandler(IDialogContext context, LuisResult result)
             {
-                await context.PostAsync("Luis intent recognized as Deposit");
+                var depositValues = new Dictionary<string, dynamic>
+                {
+                    { "Account", "" },
+                    { "TransactionAmount", 17.48 },
+                };
+                luisBankModel = new LUISBankModel();
+
+                await context.PostAsync("Luis intent recognized as Deposit");                
+                foreach (var entityRecommendation in result.Entities)
+                {
+                    if (luisBankModel.entities.Contains(entityRecommendation.Type))
+                    {
+                        var x = entityRecommendation.Entity;                        
+                    }
+                }
+                // result.Entities[0];
                 context.Wait(MessageReceived);
             }
 
---
+++ LUISBankingBot/Models/LUISBankModel.cs
@@ -0,0 +1,19 @@
+﻿namespace LUISBankingBot.Models
+{
+    using System.Collections.Generic;
+
+    public class LUISBankModel
+    {
+        public List<string> intents { get; private set; }
+        public List<string> entities { get; private set; }
+        public List<string> composites { get; private set; }
+
+        public LUISBankModel()
+        {
+            intents = new List<string>() { "Deposit", "None", "ShowBalance", "Transfer", "Withdraw" };
+            entities = new List<string>() { "Account", "AccountTo" };
+            composites = new List<string>() { "TransactionAmount" };
+        }
+
+    }
+}
